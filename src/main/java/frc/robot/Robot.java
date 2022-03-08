c0b0e9717816d0d2c955e12ae5e33df147986c8f
WAaron-500 <61605186+WAaron-500@users.noreply.github.com>
2022-03-08 18:02:42 -0500

    Revert Robot.java to original state

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -4,12 +4,9 @@
 
 package frc.robot;
 
-import frc.robot.Constants;
-
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
-import frc.robot.Constants.BotType;
 
 /**
  * The VM is configured to automatically run this class, and to call the functions corresponding to
@@ -31,9 +28,6 @@ public class Robot extends TimedRobot {
     // Instantiate our RobotContainer.  This will perform all our button bindings, and put our
     // autonomous chooser on the dashboard.
     m_robotContainer = new RobotContainer();
-
-    // Sets the desired bot to be used
-    Constants.setBot(BotType.ROBOT_2022_COMP);
   }
 
   /**
