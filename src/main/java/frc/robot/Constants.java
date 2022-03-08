47731cb55723e70bf3ad09df839bb1de3ebfb7f7
SFang-cmd <57546442+SFang-cmd@users.noreply.github.com>
2022-03-07 23:17:26 -0500

    Drive Base code with updated constants

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -12,7 +12,9 @@ package frc.robot;
  * globally (i.e. public static). Do not put anything functional in this class.
  *
  * <p>
- * It is advised to statically import this class (or one of its inner classes)
+ * It is advised to statically
+
+ import this class (or one of its inner classes)
  * wherever the
  * constants are needed, to reduce verbosity.
  */
