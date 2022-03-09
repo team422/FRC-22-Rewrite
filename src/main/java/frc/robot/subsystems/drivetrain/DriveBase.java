f4239ae4557a6379cbfdd6d3cc037b81fd787448
FellStinger <shreyassmuthusamy@gmail.com>
2022-03-09 18:44:03 -0500

    formatting

--- src/main/java/frc/robot/subsystems/drivetrain/DriveBase.java
+++ src/main/java/frc/robot/subsystems/drivetrain/DriveBase.java
@@ -7,42 +7,42 @@ import edu.wpi.first.math.controller.SimpleMotorFeedforward;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class DriveBase extends SubsystemBase {
-	// Static final for now, make sure to change in order to switch between drives
-	private static final double wheelRadiusMeters = 0.0508;
-	private static final double maxVelocityMetersPerSec = 10;
+    // Static final for now, make sure to change in order to switch between drives
+    private static final double wheelRadiusMeters = 0.0508;
+    private static final double maxVelocityMetersPerSec = 10;
 
-	private final DriveIO driveIO;
+    private final DriveIO driveIO;
 
-	private final SimpleMotorFeedforward leftFF;
-	private final SimpleMotorFeedforward rightFF;
+    private final SimpleMotorFeedforward leftFF;
+    private final SimpleMotorFeedforward rightFF;
 
-	public DriveBase(DriveIO driveIO) {
-		this.driveIO = driveIO;
+    public DriveBase(DriveIO driveIO) {
+        this.driveIO = driveIO;
 
-		// Arbitrary values for now
-		leftFF = new SimpleMotorFeedforward(0.01, 0.01);
-		rightFF = new SimpleMotorFeedforward(0.01, 0.01);
-	}
+        // Arbitrary values for now
+        leftFF = new SimpleMotorFeedforward(0.01, 0.01);
+        rightFF = new SimpleMotorFeedforward(0.01, 0.01);
+    }
 
-	public void driveVoltage(double leftVoltage, double rightVoltage) {
-		driveIO.setVoltage(leftVoltage, rightVoltage);
-	}
+    public void driveVoltage(double leftVoltage, double rightVoltage) {
+        driveIO.setVoltage(leftVoltage, rightVoltage);
+    }
 
-	public void drivePercent(double leftPercent, double rightPercent) {
-		driveVelocity(leftPercent * maxVelocityMetersPerSec,
-			rightPercent * maxVelocityMetersPerSec);
-	}
+    public void drivePercent(double leftPercent, double rightPercent) {
+        driveVelocity(leftPercent * maxVelocityMetersPerSec,
+            rightPercent * maxVelocityMetersPerSec);
+    }
 
-	public void driveVelocity(double leftVelocity, double rightVelocity) {
-		double leftVelocityRadPerSec = leftVelocity / wheelRadiusMeters;
-		double rightVelocityRadPerSec = rightVelocity / wheelRadiusMeters;
-		double leftFFValue = leftFF.calculate(leftVelocityRadPerSec);
-		double rightFFValue = rightFF.calculate(rightVelocityRadPerSec);
+    public void driveVelocity(double leftVelocity, double rightVelocity) {
+        double leftVelocityRadPerSec = leftVelocity / wheelRadiusMeters;
+        double rightVelocityRadPerSec = rightVelocity / wheelRadiusMeters;
+        double leftFFValue = leftFF.calculate(leftVelocityRadPerSec);
+        double rightFFValue = rightFF.calculate(rightVelocityRadPerSec);
 
-		driveIO.setVelocity(leftVelocityRadPerSec, rightVelocityRadPerSec, leftFFValue, rightFFValue);
-	}
+        driveIO.setVelocity(leftVelocityRadPerSec, rightVelocityRadPerSec, leftFFValue, rightFFValue);
+    }
 
-	public void setBrakeMode(boolean enable) {
-		driveIO.setBrakeMode(enable);
-	}
+    public void setBrakeMode(boolean enable) {
+        driveIO.setBrakeMode(enable);
+    }
 }
