e9abd3bc4780e8622bca73c438fad14468b613c6
FellStinger <shreyassmuthusamy@gmail.com>
2022-03-17 16:34:36 -0400

    changes to traj gen

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -4,26 +4,11 @@
 
 package frc.robot;
 
-import java.util.List;
-
-import edu.wpi.first.math.controller.PIDController;
-import edu.wpi.first.math.controller.RamseteController;
-import edu.wpi.first.math.controller.SimpleMotorFeedforward;
-import edu.wpi.first.math.geometry.Pose2d;
-import edu.wpi.first.math.geometry.Rotation2d;
-import edu.wpi.first.math.geometry.Translation2d;
-import edu.wpi.first.math.kinematics.DifferentialDriveKinematics;
-import edu.wpi.first.math.trajectory.Trajectory;
-import edu.wpi.first.math.trajectory.TrajectoryConfig;
-import edu.wpi.first.math.trajectory.TrajectoryGenerator;
-import edu.wpi.first.math.trajectory.constraint.DifferentialDriveVoltageConstraint;
 import edu.wpi.first.wpilibj.GenericHID;
 import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj2.command.Command;
-import edu.wpi.first.wpilibj2.command.RamseteCommand;
+import frc.robot.commands.AutoMotionProfiler;
 import frc.robot.commands.DriveWithJoystick;
-// import frc.robot.commands.OneCargoAuto;
-import frc.robot.commands.TestAuto;
 // import frc.robot.subsystems.drivetrain.*;
 // import edu.wpi.first.wpilibj2.command.Command;
 // import frc.robot.oi.*;
@@ -51,6 +36,8 @@ public class RobotContainer {
     private final Climber climber = new Climber(
         new ClimberIOFalcon(ClimberIOFalcon.leftClimberPort),
         new ClimberIOFalcon(ClimberIOFalcon.rightClimberPort));
+
+    private final AutoMotionProfiler profiler = new AutoMotionProfiler(drive);
 
     /**
      * The container for the robot. Contains subsystems, OI devices, and commands.
@@ -97,56 +84,6 @@ public class RobotContainer {
      * @return the command to run in autonomous
      */
     public Command getAutonomousCommand() {
-        DifferentialDriveVoltageConstraint voltageConstraint =
-            new DifferentialDriveVoltageConstraint(
-                new SimpleMotorFeedforward(
-                drive.getkS(),
-                drive.getkV(),
-                drive.getkA()),
-                drive.getKinematics(),
-                10 // max voltage (maybe make variable??)
-            );
-        
-        TrajectoryConfig config =
-            new TrajectoryConfig(
-                    drive.getMaxVelocityMetersPerSecond(),
-                    drive.getMaxAccelerationMetersPerSecondSq())
-                .setKinematics(drive.getKinematics())
-                .addConstraint(voltageConstraint);
-        
-        Trajectory trajectory = getTrajectory(config);
-
-        RamseteCommand ramseteCommand =
-            new RamseteCommand(
-                trajectory,
-                drive::getPose,
-                new RamseteController(2, 0.7),
-                new SimpleMotorFeedforward(
-                    drive.getkS(),
-                    drive.getkV(),
-                    drive.getkA()),
-                drive.getKinematics(),
-                drive::getWheelSpeeds,
-                new PIDController(drive.getkP(), 0.0, drive.getkD()),
-                new PIDController(drive.getkP(), 0.0, drive.getkD()),
-                drive::driveVoltage,
-                drive);
-        
-        drive.resetOdometry(trajectory.getInitialPose());
-
-        // An ExampleCommand will run in autonomous
-        return ramseteCommand.andThen(() -> drive.driveVoltage(0.0, 0.0));
-    }
-
-    private Trajectory getTrajectory(TrajectoryConfig config) {
-        return TrajectoryGenerator.generateTrajectory(
-            new Pose2d(0, 0, new Rotation2d()),
-            List.of(
-                new Translation2d(1, 1),
-                new Translation2d(2, -1)
-            ),
-            new Pose2d(3, 0, new Rotation2d()),
-            config
-        );
+        return profiler.getTrajectoryCommand();
     }
 }
