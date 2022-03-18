82e74426c3519bd3daa0eb9117d846151b009a21
Shorya Malani <shoryamal@gmail.com>
2022-03-18 16:23:11 -0400

    Yay indexer works

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -10,8 +10,18 @@ import frc.robot.commands.DriveWithJoystick;
 import frc.robot.commands.OneCargoAuto;
 import frc.robot.subsystems.drivetrain.*;
 import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.CommandScheduler;
 import frc.robot.oi.*;
+import frc.robot.subsystems.transversal.*;
+import frc.robot.subsystems.uptake.Uptake;
+import frc.robot.subsystems.uptake.UptakeIOSparkMax;
+import frc.robot.subsystems.colorSensor.ColorSensor;
+import frc.robot.subsystems.colorSensor.ColorSensorIO;
+import frc.robot.subsystems.colorSensor.ColorSensorIORevV3;
+import com.revrobotics.ColorSensorV3;
 
+import frc.robot.commands.operatorcommands.TeleIndexer;
+import frc.robot.commands.operatorcommands.TeleTransversal;
 /**
  * This class is where the bulk of the robot should be declared. Since
  * Command-based is a
@@ -24,13 +34,17 @@ import frc.robot.oi.*;
 public class RobotContainer {
   // The robot's subsystems and commands are defined here...
   private final DriveBase drive = new DriveBase(new DriveIOFalcon());
-
+  public final ColorSensor colorSensor = new ColorSensor(new ColorSensorIORevV3());
+  public final Transversal transversal = new Transversal(new TransversalIOSparkMax());
+  public final Uptake uptake = new Uptake(new UptakeIOSparkMax());
+  
   /**
    * The container for the robot. Contains subsystems, OI devices, and commands.
    */
   public RobotContainer() {
     // Configure the button bindings
     configureButtonBindings();
+    
   }
 
   /**
@@ -55,10 +69,14 @@ public class RobotContainer {
     
     // Define default commands here
     drive.setDefaultCommand(defaultDriveCommand);
+    // colorSensor.setDefaultCommand(this.getDefaultColorSensorCommand());
+    uptake.setDefaultCommand(new TeleIndexer(transversal, uptake, colorSensor));
 
     // Define button / command bindings here
   }
-
+  public Integer getColorProximity(){
+    return colorSensor.getProximity();
+  }
   /**
    * Use this to pass the autonomous command to the main {@link Robot} class.
    *
