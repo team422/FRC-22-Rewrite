fb40b809ace8dc0a3e9a15809b4282b8eb2f0aee
WAaron-500 <61605186+WAaron-500@users.noreply.github.com>
2022-04-04 08:50:02 -0400

    fix error

--- src/main/java/frc/robot/commands/auto/routines/TwoCargoAuto.java
+++ src/main/java/frc/robot/commands/auto/routines/TwoCargoAuto.java
@@ -10,17 +10,19 @@ import frc.robot.commands.auto.DriveStraight;
 import frc.robot.commands.operatorcommands.TeleFeed;
 import frc.robot.commands.operatorcommands.TeleIntake;
 import frc.robot.commands.operatorcommands.TeleIntakeToggle;
+import frc.robot.commands.visioncommands.PositionForHub;
 import frc.robot.subsystems.drivetrain.DriveBase;
 import frc.robot.subsystems.flywheel.VarFlyWheel;
 import frc.robot.subsystems.intake.Intake;
 import frc.robot.subsystems.transversal.Transversal;
 import frc.robot.subsystems.uptake.Uptake;
+import frc.robot.subsystems.vision.Vision;
 
 public class TwoCargoAuto extends ParallelCommandGroup {
     double DRIVE_SPEED = 0.3;
 
     public TwoCargoAuto(DriveBase drive, Intake intake, Transversal transversal, Uptake uptake,
-            VarFlyWheel flywheel) {
+            VarFlyWheel flywheel, Vision HubVision) {
         addCommands(
                 // Prepare Intake
                 sequence(
@@ -42,7 +44,7 @@ public class TwoCargoAuto extends ParallelCommandGroup {
                         new DriveStraight(drive, Units.feetToMeters(3), DRIVE_SPEED),
 
                         // Align to hub using vision
-                        new PositionForHub(vision, drive).withTimeout(3),
+                        new PositionForHub(HubVision, drive).withTimeout(3),
 
                         // Shoot cargo
                         new TeleFeed(transversal, uptake, () -> 9.0).withTimeout(5),
