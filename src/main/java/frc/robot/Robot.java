532ee4356d87229b2a50f1a146febf7f7cd5e1e5
SFang-cmd <57546442+SFang-cmd@users.noreply.github.com>
2022-04-07 06:35:29 -0400

    pre dcmp changes

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -4,6 +4,7 @@
 
 package frc.robot;
 
+import edu.wpi.first.util.net.PortForwarder;
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
@@ -29,6 +30,7 @@ public class Robot extends TimedRobot {
      */
     @Override
     public void robotInit() {
+        PortForwarder.add(5800, "gloworm.local", 5800);
         // Instantiate our RobotContainer.  This will perform all our button bindings, and put our
         // autonomous chooser on the dashboard.
         m_robotContainer = new RobotContainer();
