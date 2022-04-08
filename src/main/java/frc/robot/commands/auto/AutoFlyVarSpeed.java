c030d878df56ebb2fe5fe65ed78dcb7f28234bfa
SFang-cmd <57546442+SFang-cmd@users.noreply.github.com>
2022-04-07 22:10:22 -0400

    auto fix

---
+++ src/main/java/frc/robot/commands/auto/AutoFlyVarSpeed.java
@@ -0,0 +1,91 @@
+package frc.robot.commands.auto;
+
+import org.photonvision.targeting.PhotonPipelineResult;
+
+import edu.wpi.first.math.util.Units;
+import edu.wpi.first.wpilibj.DoubleSolenoid.Value;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.Constants;
+import frc.robot.subsystems.flywheel.VarFlyWheel;
+import frc.robot.subsystems.vision.Vision;
+import frc.robot.util.FieldUtils;
+
+public class AutoFlyVarSpeed extends CommandBase {
+    private final VarFlyWheel varFlyWheel;
+    private final Vision hubCam;
+    private final double defaultValue;
+
+    public AutoFlyVarSpeed(VarFlyWheel varFlyWheel, Vision hubCam, double defaultValue) {
+        addRequirements(varFlyWheel);
+        this.varFlyWheel = varFlyWheel;
+        this.hubCam = hubCam;
+        this.defaultValue = defaultValue;
+    }
+
+    @Override
+    public void initialize() {
+        hubCam.setLEDEnabled(true);
+    }
+
+    @Override
+    public void execute() {
+        if (varFlyWheel.get() == Value.kReverse) {
+            varFlyWheel.flyVelocity(Constants.SHOOTER_DOWN_RPM);
+        } else {
+            var result = hubCam.getLatestResult();
+            double speed = calculateSpeed(result);
+
+            varFlyWheel.flyVelocity(speed);
+        }
+    }
+
+    private double calculateSpeed(PhotonPipelineResult result) {
+        if (result == null || !result.hasTargets()) {
+            SmartDashboard.putBoolean("Hub Visible", false);
+            SmartDashboard.putBoolean("Hub In Range", false);
+            return defaultValue;
+        }
+
+        double yPos = result.getBestTarget().getPitch();
+        double distance = FieldUtils.getHubDistance(yPos, hubCam);
+
+        SmartDashboard.putBoolean("Hub Visible", true);
+        SmartDashboard.putNumber("Hub Distance", Units.metersToFeet(distance));
+        if (Constants.useVisionShot) {
+            if (distance < Units.feetToMeters(6)) {
+                return 1950;
+                // } else if (distance < Units.feetToMeters(6.5)) {
+                //     return 2000;
+            } else if (distance < Units.feetToMeters(7)) {
+                return 2050;
+            } else if (distance < Units.feetToMeters(8)) {
+                return 2150;
+            } else if (distance < Units.feetToMeters(9)) {
+                return 2300;
+            } else if (distance < Units.feetToMeters(10)) {
+                return 2500;
+            } else {
+                return 2800;
+            }
+        } else {
+            if (varFlyWheel.get() == Value.kReverse) {
+                return Constants.SHOOTER_DOWN_RPM;
+            } else {
+                return Constants.SHOOTER_UP_RPM;
+            }
+
+        }
+    }
+
+    @Override
+    public void end(boolean interrupted) {
+        hubCam.setLEDEnabled(false);
+        varFlyWheel.stop();
+    }
+
+    @Override
+    public boolean isFinished() {
+        return false;
+    }
+}
--- src/main/java/frc/robot/commands/auto/routines/TwoCargoAuto.java
+++ src/main/java/frc/robot/commands/auto/routines/TwoCargoAuto.java
@@ -3,9 +3,8 @@ package frc.robot.commands.auto.routines;
 import edu.wpi.first.math.util.Units;
 import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
 import edu.wpi.first.wpilibj2.command.WaitCommand;
-import frc.robot.Constants;
-import frc.robot.commands.RunFlyWheel;
 import frc.robot.commands.SetIntakeExtended;
+import frc.robot.commands.auto.AutoFlyVarSpeed;
 import frc.robot.commands.auto.DriveStraight;
 import frc.robot.commands.operatorcommands.TeleFeed;
 import frc.robot.commands.operatorcommands.TeleIntake;
@@ -15,12 +14,13 @@ import frc.robot.subsystems.flywheel.VarFlyWheel;
 import frc.robot.subsystems.intake.Intake;
 import frc.robot.subsystems.transversal.Transversal;
 import frc.robot.subsystems.uptake.Uptake;
+import frc.robot.subsystems.vision.Vision;
 
 public class TwoCargoAuto extends ParallelCommandGroup {
     double DRIVE_SPEED = 0.3;
 
     public TwoCargoAuto(DriveBase drive, Intake intake, Transversal transversal, Uptake uptake,
-            VarFlyWheel flywheel) {
+            VarFlyWheel flywheel, Vision hubVision) {
         addCommands(
                 // Prepare Intake
                 sequence(
@@ -34,7 +34,8 @@ public class TwoCargoAuto extends ParallelCommandGroup {
                         new TeleIntake(intake, () -> -7.0)).withTimeout(5),
 
                 // Start shooter
-                new RunFlyWheel(flywheel, Constants.SHOOTER_UP_RPM, true),
+                // new RunFlyWheel(flywheel, Constants.SHOOTER_UP_RPM, true),
+                new AutoFlyVarSpeed(flywheel, hubVision, 2150),
 
                 // Auto Drive Sequence
                 sequence(
