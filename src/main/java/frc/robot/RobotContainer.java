630abfff74b0522d49ddf4d413e4f2d06d3630ad
SFang-cmd <57546442+SFang-cmd@users.noreply.github.com>
2022-04-09 07:43:05 -0400

    Imported automode

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -9,6 +9,7 @@ import edu.wpi.first.wpilibj.GenericHID;
 import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
+import frc.robot.Constants.AutoMode;
 import frc.robot.commands.ArcadeDrive;
 import frc.robot.commands.RunFlyWheel;
 import frc.robot.commands.auto.routines.FiveCargoAuto;
